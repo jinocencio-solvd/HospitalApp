8295ae212a411cb6179e0de2749708bffd3aadbc
jinocencio-solvd <jinocencio.laba@solvd.com>
2023-06-11 13:53:57 -0700

    formatting

--- src/main/java/com/laba/models/Clinician.java
+++ src/main/java/com/laba/models/Clinician.java
@@ -3,9 +3,8 @@ package com.laba.models;
 import java.util.Objects;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute; 
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
-
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "clinician")
--- src/main/java/com/laba/models/Patient.java
+++ src/main/java/com/laba/models/Patient.java
@@ -2,7 +2,7 @@ package com.laba.models;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute; 
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
