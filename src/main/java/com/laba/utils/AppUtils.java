685765ebe7171cb17a01ffc00832442ff9623543
jinocencio-solvd <jinocencio.laba@solvd.com>
2023-06-13 14:13:50 -0700

    attempt2 for fixed server side failed tests

--- src/main/java/com/laba/utils/AppUtils.java
+++ src/main/java/com/laba/utils/AppUtils.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class AppUtils {
 
-    public static final String DB_XML_PATH = "src/main/resources/XML/hospital.xml";
+    public static final String DB_XML_PATH = "src/main/resources/xml/hospital.xml";
 
     public static Map<String, List<Map<String, String>>> getHospitalDbMapFromXml() {
         return XMLParser.domParseToMap(new File(DB_XML_PATH));
